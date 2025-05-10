92160d1e18e53be86e38db690969fc0c96a6cf0c
AbhinavAnand241201 <iabhinavanand10@gmail.com>
2025-05-11 02:08:30 +0530

    Initial Commit

---
+++ phoneS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  phoneS
+//
+//  Created by ABHINAV ANAND  on 11/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ phoneS/phoneSApp.swift
@@ -0,0 +1,17 @@
+//
+//  phoneSApp.swift
+//  phoneS
+//
+//  Created by ABHINAV ANAND  on 11/05/25.
+//
+
+import SwiftUI
+
+@main
+struct phoneSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
